76e5f2e4e4120c3ccb88ab41f9901487ea3bba46
Filip Pacurar <filip@pacurar.net>
2021-05-06 13:56:50 +0300

    accept both types of token in same service

--- service.php
+++ service.php
@@ -7,6 +7,9 @@ class Keyring_Service_Dummy extends Keyring_Service_OAuth2 {
 	const LABEL      = 'Dummy Service';
 	const API_BASE   = 'https://dummy-service.pacurar.dev/api/';
 	const OAUTH_BASE = 'https://dummy-service.pacurar.dev/oauth/';
+
+	public $supports_basic = true;
+
 	function __construct() {
 		parent::__construct();
 
@@ -166,7 +169,96 @@ class Keyring_Service_Dummy extends Keyring_Service_OAuth2 {
 		echo '</p>';
 		echo '</form>';
 		echo '</div>';
+
+		$this->http_basic_auth_ui();
 	}
+
+	protected function http_basic_auth_ui()
+    {
+        echo '<div class="wrap">';
+            echo '<h2>' . __('Account Details', 'keyring') . '</h2>';
+
+            // Handle errors
+            if (isset($_GET['error'])) {
+                echo '<div id="keyring-admin-errors" class="updated"><ul>';
+                switch ($_GET['error']) {
+                    case '401':
+                        echo '<li>' . __('Your account details could not be confirmed, please try again.', 'keyring') . '</li>';
+                        break;
+                    case 'empty':
+                        echo '<li>' . __('Please make sure you enter a username and password.', 'keyring') . '</li>';
+                        break;
+                }
+                echo '</ul></div>';
+            }
+
+            // Output basic form for collecting user/pass
+            /* translators: service name */
+            echo '<p>' . sprintf(__('Enter your username and password for accessing <strong>%s</strong>:', 'keyring'), $this->get_label()) . '</p>';
+            echo '<form method="post" action="">';
+            echo '<input type="hidden" name="service" value="' . esc_attr($this->get_name()) . '" />';
+            echo '<input type="hidden" name="action" value="manage" />';
+            wp_nonce_field('keyring-manage', 'kr_nonce', false);
+            wp_nonce_field('keyring-manage-' . $this->get_name(), 'nonce', false);
+            echo '<table class="form-table">';
+            echo '<tr><th scope="row">' . __('Email', 'keyring') . '</th>';
+            echo '<td><input type="text" name="username" value="" id="username" class="regular-text"></td></tr>';
+            echo '<tr><th scope="row">' . __('Password', 'keyring') . '</th>';
+            echo '<td><input type="password" name="password" value="" id="password" class="regular-text"></td></tr>';
+            echo '</table>';
+            echo '<p class="submitbox">';
+            echo '<input type="submit" name="submit" value="' . __('Verify Details', 'keyring') . '" id="submit" class="button-primary">';
+            echo '<a href="' . esc_url(Keyring_Util::admin_url(null, array('action' => 'services'))) . '" class="submitdelete" style="margin-left:2em;">' . __('Cancel', 'keyring') . '</a>';
+            echo '</p>';
+            echo '</form>';
+            echo '</div>';
+            ?>
+        <script type="text/javascript" charset="utf-8">
+            jQuery(document).ready(function () {
+                jQuery('#username').focus();
+            });
+        </script>
+        <?php
+
+        if (isset($_POST['username']) && isset($_POST['password'])) {
+            // HTTP Basic does not use Keyring_Request_Tokens, since there's only one step
+
+            $token = new Keyring_Access_Token(
+                $this->get_name(),
+                base64_encode(trim($_POST['username']) . ':' . trim($_POST['password']))
+            );
+			$token->meta['test_basic'] = '1';
+            $this->set_token($token);
+
+            $meta = array_merge(
+                array(
+                    'username' => trim($_POST['username']),
+                    'basic' => true,
+                    apply_filters('keyring_access_token_meta', array(), $this->get_name(), $token, null, $this),
+                )
+            );
+
+			if($this->request($this->self_url) !== null) {
+				$access_token = new Keyring_Access_Token(
+					$this->get_name(),
+					$token,
+					$meta
+				);
+				$access_token = apply_filters('keyring_access_token', $access_token, array());
+
+
+				// If we didn't get a 401, then we'll assume it's OK
+				$this->store_token($access_token);
+				echo '<div class="notice notice-success is-dismissible">
+					<p>Basic connection saved. View the connection <a href="'.admin_url('tools.php?page=keyring').'">here</a></p>
+				</div>';
+			} else {
+				echo '<div class="notice notice-error is-dismissible">
+        <p>Invalid credentials. Cannot save a new Basic connection.</p>
+    </div>';
+			}
+        }
+    }
 
     function is_configured() {
 		$creds = $this->get_credentials();
@@ -191,6 +283,20 @@ class Keyring_Service_Dummy extends Keyring_Service_OAuth2 {
 			);
 			exit;
 		}
+	}
+
+	function request( $url, array $params = array() ) {
+		if($this->token && isset($this->token->meta['test_basic'])) {
+			$this->authorization_header = 'Basic';
+			$this->token = $this->token->token;
+		}
+		else if($this->token && !is_string($this->token) && isset($this->token->meta['basic']) && $this->token->meta['basic'] == 1) {
+			if($this->token->token->token) {
+				$this->authorization_header = 'Basic';
+				$this->token = $this->token->token->token;
+			}
+		}
+		return parent::request($url, $params);
 	}
 }
 
